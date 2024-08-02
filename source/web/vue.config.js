c5a99a76f704bd45f982faa0ea47bc1abb08c55e
Simon Krol <smkrol@amazon.com>
2024-08-02 11:50:04 -0400

    Update to v1.5.9 open-source release

--- source/web/vue.config.js
+++ source/web/vue.config.js
@@ -15,5 +15,22 @@ module.exports = {
     performance: {
       hints: false
     }
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2
+            }
+          }
+        }
+      })
   }
 };
